a7705885d7e570eaf58dd51a29b4fc3be49132d5
nikiraj <nikiraj3007@gmail.com>
2017-07-21 14:05:29 +0530

    Add files via upload

--- powerr.java
+++ powerr.java
@@ -1,4 +1,33 @@
+import java.util.Scanner;
 
 public class powerr {
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			Scanner s=new Scanner(System.in);
+			int n=s.nextInt();
+			int len=String.valueOf(n).length();
+			int[] a=new int[len];
+			int i,reverse=0;
+			for(i=0;i<len;i++)
+			{
+				int rem=n%10;
+				reverse=reverse*10+rem;
+				n/=10;
+			}
+			for(i=0;i<len;i++)
+			{
+				int rem=reverse%10;
+				a[i]=rem;
+				reverse/=10;
+			}
+			
+			int sum=0;
+			for(i=0;i<len-1;i++)
+			{
+				sum=(int) (sum+Math.pow(a[i], a[i+1]));
+		
+			}
+			System.out.println(sum+a[len-1]);
+		}
 
-}
+	}
